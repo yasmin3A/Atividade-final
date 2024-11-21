51afbe3aa0da68672dbff4992935239e5d60ce90
yasmin3A <00001105948584sp@al.educacao.sp.gov.br>
2024-11-21 17:52:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'pergunta',
+    'Quem criou o mundo?',
+    'Jesus Cristo'
+)
+
+criaCartao(
+    'pergunta',
+    'Quem que comeu o funto no jardim do Éden?',
+    'Eva'
+)
+
+criaCartao(
+    'pergunta',
+    'Onde fica o Jardim do Éden de Adão e Eva?',
+    'no Golfo Pérsico; no sul da Mesopotâmia (atual Iraque), onde os rios Tigre e Eufrates correm para o mar; e na Armênia.'
+)
+
+criaCartao(
+    'pergunta',
+    'Quem abriu o mar vermelho?',
+    'Moisés estende seu cajado e Deus divide as águas do Yam Suph (Mar Vermelho).'
+)
